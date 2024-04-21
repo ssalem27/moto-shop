29f687386081c2caa5dd4061a75fc2def3e5513e
ssalem27 <safialhasi@gmail.com>
2024-04-21 17:30:45 -0400

    frontend changes

--- moto-frontend/src/App.vue
+++ moto-frontend/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   import Home from "./views/Home.vue";
   import Navbar from "./components/Navbar.vue";
+  import Infobar from "./components/Infobar.vue"
 </script>
 
 <template>
-  <Navbar/>
-  <Home/>
+    <Navbar/>
+    <Home/>
+    <Infobar/>
 </template>
 
